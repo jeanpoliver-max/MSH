c35706ddcc63fac8b1fa02e7f67192ca926852d9
jeanpoliver-max <jeanp.oliver@gmail.com>
2026-07-29 15:19:30 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/MSH/',
+    base: '/', // <--- Altere de '/MSH/' para apenas '/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
